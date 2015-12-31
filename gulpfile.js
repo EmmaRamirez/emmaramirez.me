5d80dc563b740835ee1d1658dc937b91377e32f7
Emma <ramirezerick15@gmail.com>
2015-12-31 15:53:07 -0500

    add more styling

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,16 @@ var browserSync = require('browser-sync').create();
 var babel = require('gulp-babel');
 var sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('styles', function() {
-  gulp.src('sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer({
-			browsers: ['last 2 versions']
-		}))
-    .pipe(gulp.dest('./css'));
+gulp.task('styles', () => {
+  return gulp.src('sass/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(autoprefixer({
+    			browsers: ['last 2 versions']
+    		}))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./css'));
 });
 
 gulp.task('babel', () => {
@@ -24,10 +27,12 @@ gulp.task('babel', () => {
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', function(){
-  gulp.watch('sass/**/.scss', ['styles']);
+gulp.task('watch', function(){
+  gulp.watch('sass/**/*.scss', ['styles']);
   gulp.watch('src/**/*.js', ['babel']);
 });
+
+gulp.task('default', ['styles', 'babel', 'watch']);
 
 browserSync.init({
     server: "./"
