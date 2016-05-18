665b229ba47b502ccc56e836f38f4e7ab6670735
Emma <ramirez.emma.g@gmail.com>
2016-05-18 07:04:19 -0400

    add gulp typescript

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp'),
     stylus = require('gulp-stylus'),
+    typescript = require('gulp-typescript'),
+    concat = require('gulp-concat'),
+    tsProject = typescript.createProject('tsconfig.json'),
     sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('stylus', function () {
@@ -10,6 +13,18 @@ gulp.task('stylus', function () {
           .pipe(gulp.dest('./styles/css/'));
 });
 
+gulp.task('scripts', function () {
+  var typescriptResult = tsProject.src()
+      .pipe(typescript(tsProject))
+      .pipe(sourcemaps.init())
+      .pipe(typescript({
+        sortOutput: true
+      }));
+  return typescriptResult.js.pipe(concat('bundle.js'))
+                            .pipe(sourcemaps.write())
+                            .pipe(gulp.dest('dist/js'));
+})
+
 //gulp.task('sroucemaps')
 
-gulp.task('default', ['stylus'])
+gulp.task('default', ['stylus', 'scripts']);
