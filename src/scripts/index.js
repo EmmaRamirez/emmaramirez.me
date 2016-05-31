d3e9426147f40570377f162a2df8343988562511
Emma <ramirez.emma.g@gmail.com>
2016-05-31 08:45:19 -0400

    add articles

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,12 +1,38 @@
 "use strict";
 var articles = [
     {
-        link: '',
-        title: '',
-        dateTime: '',
-        description: ''
+        link: 'posts/article-title',
+        title: 'This is an article',
+        dateTime: '2016-05-31 08:39am',
+        description: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.'
+    },
+    {
+        link: 'posts/article-title',
+        title: 'This is also an article',
+        dateTime: '2016-05-31 08:39am',
+        description: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.'
+    },
+    {
+        link: 'posts/article-title',
+        title: 'Stylus: A Master Guide',
+        dateTime: '2016-05-31 08:39am',
+        description: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.'
     }
 ];
 var settings = {
     displayDescription: true
 };
+function createArticle(article) {
+    var articleTemplate = "\n    <article class='site-article'>\n      <header>\n        <h3 class='site-article-heading'><a href='" + article.link + "'>" + article.title + "</a></h3>\n        <span class='site-article-date-time'>" + article.dateTime + "</span>\n      </header>\n      <div class='content'>\n        <p>" + article.description + "</p>\n      </div>\n    </article>";
+    return articleTemplate;
+}
+function appendArticles() {
+    var content = document.querySelector('.site-main');
+    for (var i = 0; i < articles.length; i++) {
+        content.innerHTML += createArticle(articles[i]);
+    }
+}
+function init() {
+    appendArticles();
+}
+init();
--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -2,13 +2,53 @@ import { Article } from './interface.article';
 
 let articles:Article[] = [
   {
-    link: '',
-    title: '',
-    dateTime: '',
-    description: ''
+    link: 'posts/article-title',
+    title: 'This is an article',
+    dateTime: '2016-05-31 08:39am',
+    description: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.'
+  },
+  {
+    link: 'posts/article-title',
+    title: 'This is also an article',
+    dateTime: '2016-05-31 08:39am',
+    description: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.'
+  },
+  {
+    link: 'posts/article-title',
+    title: 'Stylus: A Master Guide',
+    dateTime: '2016-05-31 08:39am',
+    description: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.'
   }
 ];
+
 
 let settings:any = {
   displayDescription: true
 };
+
+function createArticle(article:Article):string {
+  let articleTemplate = `
+    <article class='site-article'>
+      <header>
+        <h3 class='site-article-heading'><a href='${article.link}'>${article.title}</a></h3>
+        <span class='site-article-date-time'>${article.dateTime}</span>
+      </header>
+      <div class='content'>
+        <p>${article.description}</p>
+      </div>
+    </article>`;
+  return articleTemplate;
+}
+
+function appendArticles():void {
+  let content = <HTMLElement>document.querySelector('.site-main');
+  for (let i = 0; i < articles.length; i++) {
+    content.innerHTML += createArticle(articles[i]);
+  }
+}
+
+function init():void {
+  appendArticles();
+}
+
+init();
