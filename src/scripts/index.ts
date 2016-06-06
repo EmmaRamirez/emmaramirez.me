6c896705b6ae19d0815cb256e9414285c7304b10
Emma <ramirez.emma.g@gmail.com>
2016-06-06 15:34:26 -0700

    updates

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -2,34 +2,34 @@ import { Article } from './interface.article';
 
 let articles:Article[] = [
   {
-    link: 'posts/article-title',
-    title: 'This is an article',
+    link: 'posts/my-first-day-at-npmjs',
+    title: 'My First Day at npmjs',
     dateTime: '2016-05-31 08:39am',
-    description: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.',
-    tags: ['javascript', 'es6', 'css']
+    description: '',
+    tags: ['npm', 'personal']
   },
-  {
-    link: 'posts/new-article',
-    title: 'This is also an article',
-    dateTime: '2016-05-31 08:39am',
-    description: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.',
-    tags: ['webpack', 'gulp', 'video games'],
-    id: 1
-  },
-  {
-    link: 'posts/article-title',
-    title: 'Stylus: A Master Guide',
-    dateTime: '2016-05-31 08:39am',
-    description: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.',
-    tags: ['stylus', 'css']
-  },
-  {
-    link: 'posts/article-title',
-    title: 'React & Typescript: A Match Made in JS Heaven',
-    dateTime: '2016-05-31 08:39am',
-    description: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.',
-    tags: ['react', 'typescript']
-  },
+  // {
+  //   link: 'posts/new-article',
+  //   title: 'This is also an article',
+  //   dateTime: '2016-05-31 08:39am',
+  //   description: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.',
+  //   tags: ['webpack', 'gulp', 'video games'],
+  //   id: 1
+  // },
+  // {
+  //   link: 'posts/article-title',
+  //   title: 'Stylus: A Master Guide',
+  //   dateTime: '2016-05-31 08:39am',
+  //   description: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.',
+  //   tags: ['stylus', 'css']
+  // },
+  // {
+  //   link: 'posts/article-title',
+  //   title: 'React & Typescript: A Match Made in JS Heaven',
+  //   dateTime: '2016-05-31 08:39am',
+  //   description: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.',
+  //   tags: ['react', 'typescript']
+  // },
 ];
 
 
