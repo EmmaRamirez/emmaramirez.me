56f46499b690d45e869628264dd97d1fec660112
Emma Ramirez <ramirez.emma.g@gmail.com>
2017-12-25 14:48:59 -0700

    docs: update README, add semantic-release

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,24 @@ module.exports = {
     output: {
         filename: './public/bundle.js'
     },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'awesome-typescript-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.styl?u?s$/,
+                loaders: [
+                    { loader: 'style-loader' },
+                    { loader: 'css-loader' },
+                    { loader: 'resolve-url-loader' },
+                    { loader: 'stylus-loader' }
+                ]
+            }
+        ]
+    },
     plugins: [
         new copyWebpackPlugin(
             [{
