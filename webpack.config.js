b33c924d5e577562d5524f2a688f96ff6a89b7a8
Emma Ramirez <ramirez.emma.g@gmail.com>
2017-12-25 14:35:55 -0700

    fix: make webpack actually work

--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,15 @@ const copyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './src/index.ts',
+    output: {
+        filename: './public/bundle.js'
+    },
     plugins: [
         new copyWebpackPlugin(
-            {
+            [{
                 from: './src/index.html',
                 to: './public/index.html'
-            }
+            }]
         )
     ]
 }
