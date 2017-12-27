07499eb7e6decd8a3548219e1f23f5087d81d67b
Emma Ramirez <ramirez.emma.g@gmail.com>
2017-12-27 11:37:03 -0700

    test(ElsewhereLinks): add init test

---
+++ src/components/ElsewhereLinks/__tests__/ElsewhereLinks.test.ts
@@ -0,0 +1,27 @@
+import { ElsewhereLinks, ElsewhereLink } from '..';
+
+declare var describe: any;
+
+describe('ElsewhereLinks', () => {
+    const data:ElsewhereLink[] = [
+        {
+            link: 'jest',
+            name: 'test'
+        }
+    ];
+
+    it('renders', () => {
+        const links = new ElsewhereLinks(data);
+        expect(typeof links.render()).toBe('string');
+    });
+
+    it('can contain data', () => {
+        const links = new ElsewhereLinks(data);
+        const result = links.render();
+
+        // Name has three references
+        expect(result.match(/test/g)).toHaveLength(3);
+        // Link has one
+        expect(result.match(/jest/g)).toHaveLength(1);
+    });
+});
