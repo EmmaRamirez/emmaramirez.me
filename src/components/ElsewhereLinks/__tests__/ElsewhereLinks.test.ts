b08938c57422dea3ca3c80d2109e5e796051243a
Emma Ramirez <ramirez.emma.g@gmail.com>
2017-12-27 11:57:01 -0700

    fix: add some media queries

--- src/components/ElsewhereLinks/__tests__/ElsewhereLinks.test.ts
+++ src/components/ElsewhereLinks/__tests__/ElsewhereLinks.test.ts
@@ -3,25 +3,25 @@ import { ElsewhereLinks, ElsewhereLink } from '..';
 declare var describe: any;
 
 describe('ElsewhereLinks', () => {
-    const data:ElsewhereLink[] = [
-        {
-            link: 'jest',
-            name: 'test'
-        }
-    ];
+  const data: ElsewhereLink[] = [
+    {
+      link: 'jest',
+      name: 'test'
+    }
+  ];
 
-    it('renders', () => {
-        const links = new ElsewhereLinks(data);
-        expect(typeof links.render()).toBe('string');
-    });
+  it('renders', () => {
+    const links = new ElsewhereLinks(data);
+    expect(typeof links.render()).toBe('string');
+  });
 
-    it('can contain data', () => {
-        const links = new ElsewhereLinks(data);
-        const result = links.render();
+  it('can contain data', () => {
+    const links = new ElsewhereLinks(data);
+    const result = links.render();
 
-        // Name has three references
-        expect(result.match(/test/g)).toHaveLength(3);
-        // Link has one
-        expect(result.match(/jest/g)).toHaveLength(1);
-    });
+    // Name has three references
+    expect(result.match(/test/g)).toHaveLength(3);
+    // Link has one
+    expect(result.match(/jest/g)).toHaveLength(1);
+  });
 });
