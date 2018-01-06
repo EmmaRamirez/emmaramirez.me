6011c10e2ee75cca461ea11b5a1fad4efb5b2dbb
Emma Ramirez <ramirez.emma.g@gmail.com>
2018-01-05 20:12:52 -0700

    feat: finish up create new post
    Accepts --title and optional --date parameters. Writes a new markdown post to the folder that matches the date. WILL overwrite an existing post of the same title if it exists there, so be careful!

--- scripts/new-post.js
+++ scripts/new-post.js
@@ -14,33 +14,41 @@ const padZero = (s) => ('00' + s).slice(-2);
 function createNewPost(title, date) {
     const internalDate = new Date();
     let postTitle = title;
+    let postTitleDashed = postTitle.toLowerCase().replace(/\s/g, '-');
     let postDate = date != null ? date : `${padZero(internalDate.getMonth() + 1)}-${padZero(internalDate.getDate())}-${internalDate.getFullYear().toString().split('').slice(-2).join('')}`;
     let postData = data;
     let postDataArticles = postData.articles;
 
-
     postDataArticles.unshift({
-        link: './posts/' + postTitle.toLowerCase().replace(/\s/g, '-'),
+        link: './posts/' + postTitleDashed,
         title: postTitle,
         date: postDate.replace(/-/g, '/')
     });
 
     data.articles = postDataArticles;
 
-    fs.writeFile('../src/data.json', JSON.stringify(data), err => {
+    fs.mkdir(`./posts/${postDate}`, 0777, err => {
+        // We ignore errors here, since a post can still be created
+        fs.writeFile(`./posts/${postDate}/${postTitleDashed}.md`, `# ${postTitle}`, err => {
+            if (err) throw err;
+            console.log(`Created directory ${chalk.green(postDate)} with post ${chalk.cyan(postTitle)}`);
+        })
+    })
+
+    fs.writeFile('./src/data.json', JSON.stringify(data, null, 4), err => {
         if (err) throw err;
         console.log(`Wrote post ${chalk.green(postTitle)} to data.json`);
     });
-    
-    console.log(postDataArticles);
-    
-
-    
 }
 
 if (argv.title != null) {
     createNewPost(argv.title, argv.date);
 } else {
+    console.log(`
+        ${chalk.red('ERROR: ')} Please provide a title for your new post.
+
+        Run the command as ${chalk.yellow('npm run create -- --title "Post Title"')}
+    `);
     process.exitCode = 1;
     process.exit();
 }
