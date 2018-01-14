823a001b9536fa7a65173cf4176637b2681a8add
Emma Ramirez <ramirez.emma.g@gmail.com>
2018-01-14 12:48:28 -0700

    feat(drafts): enable drafting a post
    The post gets placed in the  directory (or whever config points it to). Drafted posts still appear in  but aren't emmitted into the html.

--- scripts/build.ts
+++ scripts/build.ts
@@ -8,50 +8,56 @@ const config = require('../website.config');
 
 const noop = () => {};
 
-const extension = (element:string) => {
-    const extName = path.extname(element);
-    return extName === '.md';
+const extension = (element: string) => {
+  const extName = path.extname(element);
+  return extName === '.md';
 };
 
-const ensureExists = (path:string, mask:any, cb:any) => {
-    if (typeof mask === 'function') {
-        cb = mask;
-        mask = 0o777;
+const ensureExists = (path: string, mask: any, cb: any) => {
+  if (typeof mask === 'function') {
+    cb = mask;
+    mask = 0o777;
+  }
+  fs.mkdir(path, mask, (err: any) => {
+    if (err) {
+      if (err.code === 'EEXIST') cb(null);
+      else cb(err);
+    } else {
+      cb(null);
     }
-    fs.mkdir(path, mask, (err:any) => {
-        if (err) {
-            if (err.code === 'EEXIST') cb(null);
-            else cb(err);
-        } else {
-            cb(null);
-        }
-    });
+  });
 };
 
-const isDirectory = (source:string) => fs.lstatSync(source).isDirectory();
-const getDirectories = (source:string) => fs.readdirSync(source).map((name:string) => path.join(source, name)).filter(isDirectory);
+const isDirectory = (source: string) => fs.lstatSync(source).isDirectory();
+const getDirectories = (source: string) =>
+  fs
+    .readdirSync(source)
+    .map((name: string) => path.join(source, name))
+    .filter(isDirectory);
 
 const directories = getDirectories(path.resolve(__dirname, '../posts'));
 
-directories.forEach((dir:string) => fs.readdir(path.resolve(__dirname, dir), (err:any, files:string[]) => {
+directories.forEach((dir: string) =>
+  fs.readdir(path.resolve(__dirname, dir), (err: any, files: string[]) => {
     readFiles(dir, files);
-}));
+  })
+);
 
-const readFiles = (dir:string, files:string[]) => files.filter(extension).forEach((file:string) => {
+const readFiles = (dir: string, files: string[]) =>
+  files.filter(extension).forEach((file: string) => {
     const data = fs.readFileSync(path.join(dir, file), 'utf8');
     convertToMarkdown(data, file);
-});
-
+  });
 
 marked.setOptions({
-    gfm: true,
-    tables: true,
-    smartLists: true,
-    smartypants: true
+  gfm: true,
+  tables: true,
+  smartLists: true,
+  smartypants: true
 });
 
-const buildBlogPost = (data:any, fileName:string) => {
-    return `
+const buildBlogPost = (data: any, fileName: string) => {
+  return `
 <html lang=${config.lang}>
     <head>
         <title>emmaramirez.me</title>
@@ -74,16 +80,10 @@ const buildBlogPost = (data:any, fileName:string) => {
         <div id='app'></div>
         <div id="disqus_thread"></div>
         <script>
-
-        /**
-        *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
-        *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
-        /*
         var disqus_config = function () {
-        this.page.url = 'http://emmaramirez.me/posts/${fileName}/';  // Replace PAGE_URL with your page's canonical URL variable
-        this.page.identifier = '${fileName}'; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+            this.page.url = 'http://emmaramirez.me/posts/${fileName}/';  // Replace PAGE_URL with your page's canonical URL variable
+            this.page.identifier = '${fileName}'; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
         };
-        */
         (function() { // DON'T EDIT BELOW THIS LINE
         var d = document, s = d.createElement('script');
         s.src = 'https://emmaramirez-me.disqus.com/embed.js';
@@ -105,12 +105,20 @@ const buildBlogPost = (data:any, fileName:string) => {
 </html>`;
 };
 
-const convertToMarkdown = (data:any, file:string) => {
-    const fileName = file.split('.')[0];
-    ensureExists(`./docs/posts/${fileName}`, 0o744, (err:Error) => noop());
-    const blogPost = buildBlogPost(data, fileName);
-    fs.writeFile(`./docs/posts/${fileName}/index.html`, blogPost, (err:Error) =>  {
-        if (err) console.error(err);
-        console.log(`Wrote ${chalk.yellow(fileName)} to posts ${ chalk.green(`[${new Date().toUTCString()}]`)}`);
-    });
+const convertToMarkdown = (data: any, file: string) => {
+  const fileName = file.split('.')[0];
+  ensureExists(`./docs/posts/${fileName}`, 0o744, (err: Error) => noop());
+  const blogPost = buildBlogPost(data, fileName);
+  fs.writeFile(
+    `./docs/posts/${fileName}/index.html`,
+    blogPost,
+    (err: Error) => {
+      if (err) console.error(err);
+      console.log(
+        `Wrote ${chalk.yellow(fileName)} to posts ${chalk.green(
+          `[${new Date().toUTCString()}]`
+        )}`
+      );
+    }
+  );
 };
