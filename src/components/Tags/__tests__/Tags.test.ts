1f4abd6f645f8aaaa5dea9267b9509415040534a
Emma Ramirez <ramirez.emma.g@gmail.com>
2018-01-14 00:15:38 -0700

    test: add test for Tags

--- src/components/Tags/__tests__/Tags.test.ts
+++ src/components/Tags/__tests__/Tags.test.ts
@@ -1,5 +1,15 @@
 import { Tag, Tags } from '..';
 
-describe('<Tag />', () => {});
+describe('<Tag />', () => {
+    it('renders', () => {
+        expect(Tag('tag')).toEqual(`<div class='tag'>tag</div>`);
+    });
+});
 
-describe('<Tags />', () => {});
+describe('<Tags />', () => {
+    it('renders', () => {
+        const wrapper = new Tags(['tag-a', 'tag-b']);
+        expect(wrapper.render()).toBeDefined();
+        expect(wrapper.render().match(/tag-a/g)).toBeDefined();
+    });
+});
