2a8b4ac58085c6266eeb2a8f14ee5f8293b7914f
Emma Ramirez <ramirez.emma.g@gmail.com>
2018-06-30 16:43:50 -0600

    refactor: shift docs into assets
    This means we can now destroy docs before we run npm start each time.

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
         rules: [
             {
                 test: /\.tsx?$/,
-                use: 'awesome-typescript-loader',
+                use: 'ts-loader',
                 exclude: /node_modules/
             },
             {
