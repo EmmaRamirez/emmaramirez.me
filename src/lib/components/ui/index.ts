651306c34d2805d67fbedb3301eca883c1b1bc0c
EmmaRamirez <ramirez.emma.g@gmail.com>
2025-12-09 21:25:22 -0800

    feat: export InputOTP and InputOTPGroup components
    Added exports for InputOTP and InputOTPGroup to the UI components index. This enables other parts of the application to easily import and use these OTP-related input components via the central UI barrel file.

--- src/lib/components/ui/index.ts
+++ src/lib/components/ui/index.ts
@@ -1,6 +1,9 @@
+export { default as Avatar } from './Avatar.svelte';
+export { default as Badge } from './Badge.svelte';
 export { default as Button } from './Button.svelte';
 export { default as ButtonGroup } from './ButtonGroup.svelte';
 export { default as Card } from './Card.svelte';
+export { default as Checkbox } from './Checkbox.svelte';
 export { default as Container } from './Container.svelte';
 export { default as Divider } from './Divider.svelte';
 export { default as Flex } from './Flex.svelte';
@@ -8,9 +11,14 @@ export { default as Grid } from './Grid.svelte';
 export { default as Icon } from './Icon.svelte';
 export { default as IconButton } from './IconButton.svelte';
 export { default as Input } from './Input.svelte';
+export { default as LinkButton } from './LinkButton.svelte';
 export { default as PageShell } from './PageShell.svelte';
+export { default as Radio } from './Radio.svelte';
 export { default as Section } from './Section.svelte';
+export { default as Select } from './Select.svelte';
 export { default as Slider } from './Slider.svelte';
 export { default as Stack } from './Stack.svelte';
+export { default as Switch } from './Switch.svelte';
+export { default as Textarea } from './Textarea.svelte';
 export { default as Typography } from './Typography.svelte';
 
